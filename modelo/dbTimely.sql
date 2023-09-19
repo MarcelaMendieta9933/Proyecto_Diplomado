f2f91122305fcd8432f0c9f3f2527d8f07c56b80
MarcelaMendieta9933 <marcelamendieta9933@gmail.com>
2023-09-18 23:17:01 -0500

    script creacion de base de datos

---
+++ modelo/dbTimely.sql
@@ -0,0 +1,55 @@
+-- Crear base de datos timely
+CREATE DATABASE bbdd_timely;
+
+-- Crear Tabla Usuarios
+USE bbdd_timely;
+CREATE TABLE usuarios(
+id int AUTO_INCREMENT,
+usuario VARCHAR(40),
+nivel VARCHAR(40),
+correo VARCHAR(40),
+contraseña VARCHAR(40),
+token VARCHAR(40),
+PRIMARY KEY(id)
+);
+
+-- Crear Tabla emprendedores
+USE bbdd_timely;
+CREATE TABLE emprendedores(
+id INT AUTO_INCREMENT,
+fk_id_usu INT,
+nombre_completo VARCHAR(40),
+ciudad VARCHAR(40),
+cedula VARCHAR(40),
+telefono VARCHAR(40),
+direccion VARCHAR(40),
+PRIMARY KEY(id)
+);
+
+ALTER TABLE emprendedores
+ADD COLUMN fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+-- Crear Tabla Modelo
+USE bbdd_timely;
+CREATE TABLE modelo(
+id INT AUTO_INCREMENT,
+fk_id_empre INT,
+title_emprendimiento VARCHAR(40),
+descripcion TEXT,
+productos TEXT,
+imagenes TEXT,
+whatsapp VARCHAR(40),
+logo TEXT,
+PRIMARY KEY(id)
+);
+
+-- Crear Tabla Contactanos
+USE bbdd_timely;
+CREATE TABLE contactanos(
+id INT AUTO_INCREMENT,
+nombre VARCHAR(40),
+correo VARCHAR(40),
+descripcion TEXT,
+fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
+);
