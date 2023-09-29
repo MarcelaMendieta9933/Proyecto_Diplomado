781e1145880a0c0eab40f046b010e42754adcbd9
andressandoval21 <jasandovals9112@gmail.com>
2023-09-29 00:22:18 -0500

    Quienes_somos

---
+++ vistas/quienessomos.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/quienessomos.css">
+    <link rel="icon" type="image/x-icon" href="../imagenes/logo.png"/>
+    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Quiénes Somos</title>
+   
+</head>
+<body>
+    <?php include "nav.html"; ?>
+    <div class="section">
+        <div class="title">
+            <h2>Bienvenido a Timely</h2>
+        </div>
+        <div class="content">
+            <h2>Tu Puerta a la Innovación</h2>
+            <p>En un mundo donde cada momento cuenta, surge Timely, el epicentro de la creatividad y el emprendimiento. Somos más que una plataforma; somos una comunidad vibrante de visionarios, soñadores y hacedores, unidos por nuestra pasión por el cambio y el progreso.
+            En Timely, no solo creemos en la oportunidad, sino que la creamos. Con un enfoque audaz hacia los desafíos del mañana, impulsamos las ideas más brillantes y visionarias para convertirlas en una realidad palpable. Ya sea que estés dando tus primeros pasos como emprendedor o liderando una empresa establecida, Timely es tu socio confiable en este viaje hacia el éxito.</p>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="content">
+            <h2>¿Qué Nos Hace Únicos?</h2>
+            <ul>
+                <li>Innovación en Tiempo Real: En Timely, entendemos la importancia del tiempo en el mundo de los negocios. Nos especializamos en proporcionar soluciones innovadoras en tiempo real, impulsando tu empresa hacia adelante cuando más importa.</li>
+                <li>Conexiones Significativas: Más allá de los negocios, creamos conexiones significativas entre personas con ideas afines. Desde mentores inspiradores hasta colaboradores apasionados, en Timely encontrarás una red que te impulsa a alcanzar tus metas.</li>
+                <li>Crecimiento Sostenible: Valoramos la sostenibilidad tanto como valoramos el crecimiento. Ayudamos a los emprendedores a construir negocios sostenibles que no solo prosperan en el presente, sino que también allanan el camino para las generaciones futuras.</li>
+            </ul>
+            <p>
+
+            
+
+            </p>
+        </div>
+        <div class="title">
+            <img src="../imagenes/ayuda.jpg" alt="ayuda">
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="content">
+            <h2>Tu Viaje Empieza Hoy</h2>
+            <p>¿Estás listo para transformar tus sueños en una realidad o llevar tu negocio al próximo nivel? Entonces, estás en el lugar correcto. Únete a nosotros en Timely y descubre un mundo donde las oportunidades no tienen límites y donde el tiempo está de tu lado.
+            En Timely, el futuro es ahora. Tu viaje empieza hoy..</p>
+        </div>
+        <div class="title">
+            <img src="../imagenes/viajes.jpg" alt="viajes">
+        </div>
+    </div>
+    
+
+    <div class="video-section">
+        <!-- Aquí puedes insertar tu video -->
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <?php include "footer.html"; ?>
+</body>
+</html>
