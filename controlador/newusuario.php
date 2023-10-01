f01a68c5bd39a5b6ee6185c16beeaddbc27c0901
andressandoval21 <jasandovals9112@gmail.com>
2023-09-30 20:38:20 -0500

    formulariAdmi

---
+++ controlador/newusuario.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<head>
+<script src="dist/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link rel="icon" type="image/x-icon" href="../imagenes/logo.png"/>
+</head>
+
+</html>
+
+
+<?php
+include 'conexion_db.php';
+
+// Obtiene los datos del formulario
+
+$usuario = $_POST['usuario'];
+$correo = $_POST['correo'];
+$nivel = $_POST['nivel'];
+$contraseña = $_POST['contraseña'];
+
+$queryusuario = mysqli_query($conn,"SELECT * FROM usuarios WHERE usuario = '$usuario'");
+$nr = mysqli_num_rows($queryusuario); 
+
+if($nr == 0){
+    $pass_fuerte = password_hash($contraseña, PASSWORD_BCRYPT);
+    // Inserta los datos en la base de datos
+    $sq = "INSERT INTO usuarios (usuario , correo, nivel,contraseña) VALUES ('$usuario', '$correo','$nivel','$pass_fuerte')";
+    
+    if ($conn->query($sq) === TRUE) {
+        echo "<script> Swal.fire(
+          'Usuario creado',
+          'Exitosamenete!!!',
+          'success'
+        );    
+        setTimeout(function() {
+          window.location='../vistas/pagina_administrador.php';
+        }, 1500);
+        </script>";
+    } else {
+        echo "Error: " . $sq . "<br>" . $conn->error;
+    }
+    // Cierra la conexión
+    $conn->close();
+    
+}else{
+    echo "<script> alert('No puedes registrar a este usuario: $usuario');window.location= '../vistas/pagina_administrador.php' </script>";
+}
+?>
+
+
