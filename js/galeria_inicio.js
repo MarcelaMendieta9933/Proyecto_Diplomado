bb0e740642208ea1c6870a2617e057da6d2f96a4
MarcelaMendieta9933 <marcelamendieta9933@gmail.com>
2023-10-08 00:02:47 -0500

    estilo galeria en inicio, cambio de ruta logos emprendimientos

--- js/galeria_inicio.js
+++ js/galeria_inicio.js
@@ -1,15 +1,16 @@
-// script.js
-window.addEventListener('DOMContentLoaded', (event) => {
-    fetch('../controlador/cargar_emprendimientos.php')
-    .then(response => response.json())
-    .then(data => {
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+        // Lógica para cargar los elementos de la galería
+        const response = await fetch('../controlador/cargar_emprendimientos.php');
+        const data = await response.json();
+
         const emprendimientosDiv = document.getElementById('emprendimientos');
         data.forEach(emprendimiento => {
             const emprendimientoDiv = document.createElement('div');
             emprendimientoDiv.classList.add('card', 'gallery-item', 'col-xs-12', 'col-sm-6', 'col-md-4', 'col-lg-3');
             emprendimientoDiv.setAttribute('data-tags', emprendimiento.categorias); // Si tienes categorías, puedes asignarlas aquí
             emprendimientoDiv.innerHTML = `
-                <img src="../imagenes/${emprendimiento.logo}" class="card-img-top" alt="${emprendimiento.title_emprendimiento}">
+                <img src="../imagenes/empredimientos/${emprendimiento.logo}" class="card-img-top" id="img_size" alt="${emprendimiento.categorias}">
                 <div class="card-body">
                     <h2 class="card-title">${emprendimiento.title_emprendimiento}</h2>
                     <p class="card-text">${emprendimiento.descripcion.substring(0, 100)}...</p>
@@ -18,6 +19,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
             `;
             emprendimientosDiv.appendChild(emprendimientoDiv);
         });
-    });
- });
- 
+
+        // Lógica para habilitar la funcionalidad de búsqueda
+        const searchInput = document.getElementById("search-input");
+        const galleryItems = document.querySelectorAll(".gallery-item");
+
+        searchInput.addEventListener("input", function () {
+            const searchTerm = searchInput.value.toLowerCase();
+
+            galleryItems.forEach(function (item) {
+                const altText = item.querySelector("img").getAttribute("alt").toLowerCase();
+
+                if (altText.includes(searchTerm)) {
+                    item.style.display = "block";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos: ", error);
+    }
+});
